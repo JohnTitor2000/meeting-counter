291acbd1415bdc5f9c1c1166de8c2adcd30034e9
Vitalii Uriku <johntitor2000@yandex.ru>
2023-09-10 20:14:34 +0300

    Добавлена возможность гибко создавать инлайны

--- node/src/main/java/ru/tinkoff/service/utils/KeyBoardUtils.java
+++ node/src/main/java/ru/tinkoff/service/utils/KeyBoardUtils.java
@@ -1,7 +1,28 @@
 package ru.tinkoff.service.utils;
 
 import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Component
 public class KeyBoardUtils {
+    public InlineKeyboardMarkup createInlineKeyboardButtons(int rows, int buttonInRows, String[] buttonsText, String[] callbackData) {
+        InlineKeyboardMarkup inlineKeyboardMarkup = new InlineKeyboardMarkup();
+        List<List<InlineKeyboardButton>> rowsInLine = new ArrayList<>();
+        for (int i = 1; i <= rows; i++) {
+            List<InlineKeyboardButton> rowInLine = new ArrayList<>();
+            for (int j = 1; j <= buttonInRows; j++) {
+                InlineKeyboardButton button = new InlineKeyboardButton();
+                button.setText(buttonsText[i * j - 1]);
+                button.setCallbackData(callbackData[i * j - 1]);
+                rowInLine.add(button);
+            }
+            rowsInLine.add(rowInLine);
+        }
+        inlineKeyboardMarkup.setKeyboard(rowsInLine);
+        return inlineKeyboardMarkup;
+    }
 }
